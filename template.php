68dfe5d48d6114deb49d448f99321295d76d801f
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-09 08:19:28 -0400

    Missing section header

--- template.php
+++ template.php
@@ -96,6 +96,7 @@ $(&#039;.js--example-2-open-media-modal&#039;).click( function( event ) {
 		<h4>In-page Markup</h4>
 <pre><code class="language-html">&lt;button class=&quot;js--example-2-open-media-modal&quot;&gt;Open a modal&lt;/button&gt;
 </code></pre>
+		<h4>LIVE EXAMPLE</h4>
 		<div class="live-example">
 			<button class="js--example-2-open-media-modal">Open a modal</button>
 			<script type="text/template" id="tmpl-modal-content">
