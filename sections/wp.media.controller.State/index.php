76c47eee14958f3bc87590a19341b86fe76e8124
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-10 11:33:32 -0400

    State docs start

---
+++ sections/wp.media.controller.State/index.php
@@ -0,0 +1,16 @@
+<h3>wp.media.controller.State</h3>
+<p>Extend directly from Backbone.Model.</p>
+<p>States control the modes of regions within the frame.</p>
+
+<div class="example">
+	<h3>Example</h3>
+	<h4>LIVE EXAMPLE</h4>
+	<div class="live-example">
+	</div>
+	<h4>Template Markup</h4>
+<pre><code class="language-html"></code></pre>
+	<h4>Javascript</h4>
+<pre><code class="language-javascript"></code></pre>
+	<h4>In-page Markup</h4>
+<pre><code class="language-html"></code></pre>
+</div>
