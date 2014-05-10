96ad37b793ae319a57db71fe88a2fad290dc94c9
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-10 10:41:17 -0400

    Begin StateMachine with a description.

---
+++ sections/wp.media.controller.StateMachine/index.php
@@ -0,0 +1,14 @@
+<h3>wp.media.controller.StateMachine</h3>
+<p>A <strong>state machine</strong> includes a collection of <a href="admin.php?page=media-guide&section=wp.media.controller.State">states</a>. The machine controls statefulness: one state can be active at anytime, and it can change states.</p>
+<div class="example">
+	<h3>Example</h3>
+	<h4>LIVE EXAMPLE</h4>
+	<div class="live-example">
+	</div>
+	<h4>Template Markup</h4>
+<pre><code class="language-html"></code></pre>
+	<h4>Javascript</h4>
+<pre><code class="language-javascript"></code></pre>
+	<h4>In-page Markup</h4>
+<pre><code class="language-html"></code></pre>
+</div>
