dafb96fa4a477adc61caa6d861296d143d4f2d28
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-10 09:22:21 -0400

    Bring back a basic region example

--- script.js
+++ script.js
@@ -55,6 +55,67 @@
 
 	// Example 3: media.controller.Region
 	$(document).ready( function() {
+		$('.js--example-3--render-region').click( function( event ) {
+			event.preventDefault();
+
+			// A region requires a parent view to live inside.
+			var RegionParentViewConstructor = wp.Backbone.View.extend({
+				// When the view is initialized, bind events to callbacks.
+				initialize: function() {
+					// Regions trigger events on their parent views, which
+					// the parent view should bind callbacks for.
+
+					// When the region is created:
+					// Events triggered: {region-id}:create and {region-id}:create:{mode}
+					this.on( 'region-1:create', this.onCreateRegion, this );
+
+					// When the region is rendered:
+					// Events triggered: {region-id}:render and {region-id}:render:{mode}
+				},
+
+				/**
+				 * On the "create" event, the region controller is passed as an
+				 * argument.
+				 *
+				 * This is the time to create a view on the region.
+				 */
+				onCreateRegion: function( region ) {
+					// Create a basic view constructor that binds to a template.
+					var RegionViewConstructor = wp.Backbone.View.extend({
+						// assign a compiled template function.
+						template: wp.template( 'example-3-view-1' )
+					});
+					// Create the view for the region, which is automatically
+					// rendered later.
+					region.view = new RegionViewConstructor();
+				}
+			});
+			// Create an instance of the RegionParentView.
+			var RegionParentView = new RegionParentViewConstructor({
+				// Tie the view to an existing DOM element.
+				el: '.example-3--region-parent-view'
+			});
+			// Render the region parent view.
+			RegionParentView.render();
+
+			// Create a new region
+			var RegionOne = new wp.media.controller.Region({
+				// Unique identifier.
+				id: 'region-1',
+				// The region's parent view.
+				view: RegionParentView,
+				// The selector for the element in the parent view's markup
+				// that represents the region.
+				selector: '.region-1'
+			});
+			// Render a mode on the region to trigger the {region}:create
+			// event on the parent view.
+			RegionOne.render( 'some-mode' );
+		});
+	});
+
+	// Example 4: media.controller.Region
+	$(document).ready( function() {
 		// A region requires a parent view to live inside.
 		var RegionParentViewConstructor = wp.Backbone.View.extend({
 			// When the view is initialized, bind events to callbacks.
@@ -83,7 +144,7 @@
 				// Create a basic view constructor that binds to a template.
 				var RegionViewConstructor = wp.Backbone.View.extend({
 					// assign a compiled template function.
-					template: wp.template( 'example-3-view-1' )
+					template: wp.template( 'example-4-view-1' )
 				});
 				// Create the view for the region, which is automatically
 				// rendered later.
@@ -97,7 +158,7 @@
 				// Create a basic view constructor that binds to a template.
 				var RegionViewConstructor = wp.Backbone.View.extend({
 					// assign a compiled template function.
-					template: wp.template( 'example-3-view-2' )
+					template: wp.template( 'example-4-view-2' )
 				});
 				// Create the view for the region, which is automatically
 				// rendered later.
@@ -107,7 +168,7 @@
 		// Create an instance of the RegionParentView.
 		var RegionParentView = new RegionParentViewConstructor({
 			// Tie the view to an existing DOM element.
-			el: '.example-3--region-parent-view'
+			el: '.example-4--region-parent-view'
 		});
 		// Render the region parent view.
 		RegionParentView.render();
@@ -122,13 +183,13 @@
 			// that represents the region.
 			selector: '.region-1'
 		});
-		$('.js--example-3--switch-region-to-a-mode').click( function( event ) {
+		$('.js--example-4--switch-region-to-a-mode').click( function( event ) {
 			event.preventDefault();
 			// Trigger a mode change on the region, which will hit callbacks
 			// on the RegionParentView ( RegionParentView.onCreateRegionInAMode() ).
 			RegionOne.mode( 'a-mode' );
 		});
-		$('.js--example-3--switch-region-to-b-mode').click( function( event ) {
+		$('.js--example-4--switch-region-to-b-mode').click( function( event ) {
 			event.preventDefault();
 			RegionOne.mode( 'b-mode' );
 		});
--- template.php
+++ template.php
@@ -112,27 +112,109 @@ $(&#039;.js--example-2--open-media-modal&#039;).click( function( event ) {
 	<p>Regions are not a WordPress creation: Marionette has a <a href="https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.region.md">region object</a>; Derick Bailey <a href="http://lostechies.com/derickbailey/2011/12/12/composite-js-apps-regions-and-region-managers/">wrote about the concept a while ago</a>.</p>
 	<p>A <strong>mode</strong> applies a transformed state to a region.</p>
 	<div class="example">
-		<h3>Example: Render a view in a region in two modes.</h3>
-		<p>One region is created. A callback is bound to clicking either button, which triggers a mode switch on the region, filling in the region with a different view for each mode.
-		<h4>Live Example</h4>
+		<h3>Example: Render a view in a region</h3>
+
+		<h4>LIVE EXAMPLE</h4>
 		<div class="live-example">
 			<script type="text/template" id="tmpl-example-3-view-1">
-				<h1>Hi, I&#39;m a view inside a region in "a" mode!</h1>
-			</script>
-			<script type="text/template" id="tmpl-example-3-view-2">
-				<h1>Hi, I&#39;m a view inside a region in "b" mode!</h1>
+				<h1>Hi, I&#39;m a view inside a region!</h1>
 			</script>
 			<div class="example-3--region-parent-view">
 				<div class="region-1"></div>
 			</div>
-			<button class="js--example-3--switch-region-to-a-mode">Switch the region to "a" mode</button>
-			<button class="js--example-3--switch-region-to-b-mode">Render the region in "b" mode</button>
+			<button class="js--example-3--render-region">Render a view inside a region</button>
 		</div>
 		<h4>Template Markup</h4>
 <pre><code class="language-html">&lt;script type=&quot;text/template&quot; id=&quot;tmpl-example-3-view-1&quot;&gt;
+	&lt;h1&gt;Hi, I&amp;#39;m a view inside a region!&lt;/h1&gt;
+&lt;/script&gt;</code></pre>
+		<h4>Javascript</h4>
+<pre><code class="language-javascript">$(&#039;.js--example-3--render-region&#039;).click( function( event ) {
+		event.preventDefault();
+
+		// A region requires a parent view to live inside.
+		var RegionParentViewConstructor = wp.Backbone.View.extend({
+			// When the view is initialized, bind events to callbacks.
+			initialize: function() {
+				// Regions trigger events on their parent views, which
+				// the parent view should bind callbacks for.
+
+				// When the region is created:
+				// Events triggered: {region-id}:create and {region-id}:create:{mode}
+				this.on( &#039;region-1:create&#039;, this.onCreateRegion, this );
+
+				// When the region is rendered:
+				// Events triggered: {region-id}:render and {region-id}:render:{mode}
+			},
+
+			/**
+			 * On the &quot;create&quot; event, the region controller is passed as an
+			 * argument.
+			 *
+			 * This is the time to create a view on the region.
+			 */
+			onCreateRegion: function( region ) {
+				// Create a basic view constructor that binds to a template.
+				var RegionViewConstructor = wp.Backbone.View.extend({
+					// assign a compiled template function.
+					template: wp.template( &#039;example-3-view-1&#039; )
+				});
+				// Create the view for the region, which is automatically
+				// rendered later.
+				region.view = new RegionViewConstructor();
+			}
+		});
+		// Create an instance of the RegionParentView.
+		var RegionParentView = new RegionParentViewConstructor({
+			// Tie the view to an existing DOM element.
+			el: &#039;.example-3--region-parent-view&#039;
+		});
+		// Render the region parent view.
+		RegionParentView.render();
+
+		// Create a new region
+		var RegionOne = new wp.media.controller.Region({
+			// Unique identifier.
+			id: &#039;region-1&#039;,
+			// The region&#039;s parent view.
+			view: RegionParentView,
+			// The selector for the element in the parent view&#039;s markup
+			// that represents the region.
+			selector: &#039;.region-1&#039;
+		});
+		// Render a mode on the region to trigger the {region}:create
+		// event on the parent view.
+		RegionOne.render( &#039;some-mode&#039; );
+	});
+});</code></pre>
+		<h4>In-page Markup</h4>
+<pre><code class="language-html">&lt;div class=&quot;example-3--region-parent-view&quot;&gt;
+	&lt;div class=&quot;region-1&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+&lt;button class=&quot;js--example-3--render-region&quot;&gt;Render Region&lt;/button&gt;</code></pre>
+	</div>
+	<div class="example">
+		<h3>Example: Render a view in a region in two modes.</h3>
+		<p>One region is created. A callback is bound to clicking either button, which triggers a mode switch on the region, filling in the region with a different view for each mode.
+		<h4>Live Example</h4>
+		<div class="live-example">
+			<script type="text/template" id="tmpl-example-4-view-1">
+				<h1>Hi, I&#39;m a view inside a region in "a" mode!</h1>
+			</script>
+			<script type="text/template" id="tmpl-example-4-view-2">
+				<h1>Hi, I&#39;m a view inside a region in "b" mode!</h1>
+			</script>
+			<div class="example-4--region-parent-view">
+				<div class="region-1"></div>
+			</div>
+			<button class="js--example-4--switch-region-to-a-mode">Switch the region to "a" mode</button>
+			<button class="js--example-4--switch-region-to-b-mode">Render the region in "b" mode</button>
+		</div>
+		<h4>Template Markup</h4>
+<pre><code class="language-html">&lt;script type=&quot;text/template&quot; id=&quot;tmpl-example-4-view-1&quot;&gt;
 	&lt;h1&gt;Hi, I&amp;#39;m a view inside a region in &quot;a mode&quot;!&lt;/h1&gt;
 &lt;/script&gt;
-&lt;script type=&quot;text/template&quot; id=&quot;tmpl-example-3-view-2&quot;&gt;
+&lt;script type=&quot;text/template&quot; id=&quot;tmpl-example-4-view-2&quot;&gt;
 	&lt;h1&gt;Hi, I&amp;#39;m a view inside a region in &quot;b mode&quot;!&lt;/h1&gt;
 &lt;/script&gt;</code></pre>
 		<h4>Javascript</h4>
@@ -164,7 +246,7 @@ var RegionParentViewConstructor = wp.Backbone.View.extend({
 		// Create a basic view constructor that binds to a template.
 		var RegionViewConstructor = wp.Backbone.View.extend({
 			// assign a compiled template function.
-			template: wp.template( &#039;example-3-view-1&#039; )
+			template: wp.template( &#039;example-4-view-1&#039; )
 		});
 		// Create the view for the region, which is automatically
 		// rendered later.
@@ -178,7 +260,7 @@ var RegionParentViewConstructor = wp.Backbone.View.extend({
 		// Create a basic view constructor that binds to a template.
 		var RegionViewConstructor = wp.Backbone.View.extend({
 			// assign a compiled template function.
-			template: wp.template( &#039;example-3-view-2&#039; )
+			template: wp.template( &#039;example-4-view-2&#039; )
 		});
 		// Create the view for the region, which is automatically
 		// rendered later.
@@ -188,7 +270,7 @@ var RegionParentViewConstructor = wp.Backbone.View.extend({
 // Create an instance of the RegionParentView.
 var RegionParentView = new RegionParentViewConstructor({
 	// Tie the view to an existing DOM element.
-	el: &#039;.example-3--region-parent-view&#039;
+	el: &#039;.example-4--region-parent-view&#039;
 });
 // Render the region parent view.
 RegionParentView.render();
@@ -203,22 +285,22 @@ var RegionOne = new wp.media.controller.Region({
 	// that represents the region.
 	selector: &#039;.region-1&#039;
 });
-$(&#039;.js--example-3--render-region-in-a-mode&#039;).click( function( event ) {
+$(&#039;.js--example-4--render-region-in-a-mode&#039;).click( function( event ) {
 	event.preventDefault();
 	// Render a mode on the region to trigger the {region}:create
 	// event on the parent view.
 	RegionOne.render( &#039;a-mode&#039; );
 });
-$(&#039;.js--example-3--render-region-in-b-mode&#039;).click( function( event ) {
+$(&#039;.js--example-4--render-region-in-b-mode&#039;).click( function( event ) {
 	event.preventDefault();
 	RegionOne.render( &#039;b-mode&#039; );
 });</code></pre>
 		<h4>In-page Markup</h4>
-<pre><code class="language-html">&lt;div class=&quot;example-3--region-parent-view&quot;&gt;
+<pre><code class="language-html">&lt;div class=&quot;example-4--region-parent-view&quot;&gt;
 	&lt;div class=&quot;region-1&quot;&gt;&lt;/div&gt;
 &lt;/div&gt;
-&lt;button class=&quot;js--example-3--render-region-in-a-mode&quot;&gt;Render the region in &quot;a mode&quot;&lt;/button&gt;
-&lt;button class=&quot;js--example-3--render-region-in-b-mode&quot;&gt;Render the region in &quot;b mode&quot;&lt;/button&gt;</code></pre>
+&lt;button class=&quot;js--example-4--render-region-in-a-mode&quot;&gt;Render the region in &quot;a mode&quot;&lt;/button&gt;
+&lt;button class=&quot;js--example-4--render-region-in-b-mode&quot;&gt;Render the region in &quot;b mode&quot;&lt;/button&gt;</code></pre>
 	</div>
 	<div class="entry-template">
 		<h3>TITLE</h3>
