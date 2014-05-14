c93f55cf26569eba0a86ee25b62be154c079e3f4
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-14 08:23:12 -0400

    Example title

--- sections/wp.media.controller.StateMachine/index.php
+++ sections/wp.media.controller.StateMachine/index.php
@@ -1,7 +1,7 @@
 <h3>wp.media.controller.StateMachine</h3>
 <p>A <strong>state machine</strong> includes a collection of <a href="admin.php?page=media-guide&section=wp.media.controller.State">states</a>. The machine controls statefulness: one state can be active at anytime, and it can change states.</p>
 <div class="example">
-	<h3>Example: title</h3>
+	<h3>Example: A state machine which can toggle between two states.</h3>
 	<h4>Live Example <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
 	<iframe class="iframe-interactive-demo" src="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>"></iframe>
 	<h4>Markup</h4>
