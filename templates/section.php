ea22f4bbfe27d567611cc5512931b56bc89ebc36
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-13 21:54:07 -0400

    remove unused template

--- templates/section.php
+++ templates/section.php
@@ -2,20 +2,4 @@
 	<h2>WordPress Media Backbone Guide</h2>
 	<?php require( plugin_dir_path( __FILE__ ) . 'menu.php' ); ?>
 	<?php require( WPMT::get_instance()->directory->sections . WPMT::get_current_section() . '/index.php' ); ?>
-	<div class="entry-template">
-		<h3>Title</h3>
-		<p>Content</p>
-		<div class="example">
-			<h3>Example: title</h3>
-			<h4>Live Example</h4>
-			<div class="live-example">
-			</div>
-			<h4>Template Markup</h4>
-	<pre><code class="language-html"></code></pre>
-			<h4>Javascript</h4>
-	<pre><code class="language-javascript"></code></pre>
-			<h4>In-page Markup</h4>
-	<pre><code class="language-html"></code></pre>
-		</div>
-	</div>
 </div>
