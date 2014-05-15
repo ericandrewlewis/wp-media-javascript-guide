e275b41c6fbdd1b69b85120eca0c41900214dd77
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-05-15 07:28:59 -0400

    Improve state machine documentation and see also links

--- sections/wp.media.controller.StateMachine/index.php
+++ sections/wp.media.controller.StateMachine/index.php
@@ -1,5 +1,12 @@
 <h3>wp.media.controller.StateMachine</h3>
-<p>A <strong>state machine</strong> includes a collection of <a href="admin.php?page=media-guide&section=wp.media.controller.State">states</a>. The machine controls statefulness: one state can be active at anytime, and it can change states.</p>
+<p>A <strong>finite-state machine</strong>, in general terms, is in one of a finite number of states at any given time.</p>
+<p>The <strong>StateMachine</strong> object includes a collection of <a href="admin.php?page=media-guide&section=wp.media.controller.State">states</a>. A state can specify activate and deactivate event handlers.</p>
+<h4>See also</h4>
+<ul>
+	<li><a href="http://en.wikipedia.org/wiki/Finite-state_machine">Finite-state machine</a></li>
+	<li><a href="http://en.wikipedia.org/wiki/State_pattern">State pattern</a></li>
+	<li><a href="http://robdodson.me/blog/2012/06/02/take-control-of-your-app-with-the-javascript-state-patten">Rob Dodson, Take Control of Your App With the JavaScript State Pattern</a></li>
+</ul>
 <div class="example">
 	<h3>Example: A state machine which can toggle between two states.</h3>
 	<h4>Live Example <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
