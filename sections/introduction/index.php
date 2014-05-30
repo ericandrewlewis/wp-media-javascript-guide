e2bad6176ac37757086622b33c07b93c1afe0f99
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-05-30 15:03:49 -0400

    Copy changes.

--- sections/introduction/index.php
+++ sections/introduction/index.php
@@ -1,3 +1,8 @@
 <h3>An introduction to Backbone design patterns and reusable elements in the WordPress Media experience.</h3>
-<p>Backbone is an unopinionated framework to build with, meaning it provides basic objects to build with, while leaving architectural and implementation decisions up to the developer. While liberating, this means peculiarities can be built into a codebase that make the code hard to grok. The solution to this? Documentation. </p>
-<p>Some knowledge of <a target="_blank" href="http://backbonejs.org">Backbone</a> is a good idea. Addy Osmani's <a target="_blank" href="http://addyosmani.github.io/backbone-fundamentals/">Developing Backbone.js Applications</a> is a solid primer.</p>
+<p>Backbone is an unopinionated framework to build on top of. It provides basic
+	objects (models and views are important to us), while leaving architectural
+	decisions to the developer. Peculiarities can be built into a codebase that
+	make the code hard to grok. The solution to this? Documentation. </p>
+<p>Some knowledge of <a target="_blank" href="http://backbonejs.org">Backbone</a>
+	is a good idea. Addy Osmani's <a target="_blank" href="http://addyosmani.github.io/backbone-fundamentals/">Developing Backbone.js Applications</a>
+	is a solid primer.</p>
