4d62d7f6be584e8fc7991949613d88cb0709e534
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-06-01 12:24:01 -0400

    COpy

--- sections/wp.media.View.Modal/index.php
+++ sections/wp.media.View.Modal/index.php
@@ -1,5 +1,6 @@
 <h3>wp.media.View.Modal</h3>
-<p>A view that creates a modal. The modal can be closed and reopened without changing markup (i.e. losing state). The default media experience uses it as a wrapper, however it can be used outside the media context.</p>
+<p>A modal that can be filled with any markup. The modal can be closed and reopened without the markup changing (i.e. losing state).</p>
+<p>The media experience uses it as a container.</p>
 <div class="example">
 	<h3>Example: Open a modal</h3>
 	<h4>LIVE EXAMPLE <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
