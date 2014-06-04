650e1aa70a83aedd2957f0b89acbcb8bcbf1e832
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-06-04 12:29:15 -0400

    Koop video on intro page

--- sections/Introduction/index.php
+++ sections/Introduction/index.php
@@ -6,3 +6,7 @@
 <p>Some knowledge of <a target="_blank" href="http://backbonejs.org">Backbone</a>
 	is a good idea. Addy Osmani's <a target="_blank" href="http://addyosmani.github.io/backbone-fundamentals/">Developing Backbone.js Applications</a>
 	is a solid primer.</p>
+<figure>
+	<iframe width="560" height="315" src="//www.youtube.com/embed/j5KPXLzuBXE" frameborder="0" allowfullscreen></iframe><a href="https://twitter.com/koop">
+	<figcaption>Daryl Koopersmith</a> who wrote most of the Media code giving a brief introduction to Underscore, Backbone, and wp.media. Worth a watch.</figcaption>
+</figure>
