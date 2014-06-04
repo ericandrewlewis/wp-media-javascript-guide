4d3345b9046bcda16399a300935a3f5242b8d252
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-06-04 17:35:54 -0400

    copy

--- sections/wp.media.controller.StateMachine/index.php
+++ sections/wp.media.controller.StateMachine/index.php
@@ -2,6 +2,15 @@
 <p>A <a href="http://en.wikipedia.org/wiki/Finite-state_machine">finite-state machine</a> is in one of a number of states at any given time.</p>
 <p>All media frames (subclasses of <code>media.view.Frame</code>) are mixins that include a <code>StateMachine</code>.</p>
 <p>Subclasses of <a href="admin.php?page=media-guide&section=wp.media.controller.State">wp.media.controller.State</a> are added to a frame.</p>
+<h3>Methods</h3>
+<dl>
+	<dt><code>state( state )</code></dt>
+	<dd>Get a state object. <br>Defaults to the current state; if a state ID is supplied, returns that state.</dd>
+	<dt><code>setState( state )</code></dt>
+	<dd>Set the state.<br>
+		Triggers an `activate` event on the state, and a `deactivate` event on the previous state.</dd>
+	<dt><code>lastState()</code></dt>
+	<dd>Get the previously active state object.</dd>
 <div class="example">
 	<h3>Example: A simple state machine that toggles between two states.</h3>
 	<h4>Live Example <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
