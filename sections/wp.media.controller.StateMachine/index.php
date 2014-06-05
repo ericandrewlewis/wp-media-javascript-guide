c63d07539cf3b58b038c36bdeb9190a47b29cefd
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-06-05 19:06:09 -0400

    copy

--- sections/wp.media.controller.StateMachine/index.php
+++ sections/wp.media.controller.StateMachine/index.php
@@ -11,6 +11,7 @@
 		Triggers an `activate` event on the state, and a `deactivate` event on the previous state.</dd>
 	<dt><code>lastState()</code></dt>
 	<dd>Get the previously active state object.</dd>
+</dl>
 <div class="example">
 	<h3>Example: A simple state machine that toggles between two states.</h3>
 	<h4>Live Example <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
--- sections/wp.media.view.MediaFrame.Select/index.php
+++ sections/wp.media.view.MediaFrame.Select/index.php
@@ -1,5 +1,30 @@
 <h3>wp.media.view.MediaFrame.Select</h3>
-<p>A workflow for choosing a single attachment from the media library.</p>
+<p>A workflow dffor choosing a single attachment from the media library.</p>
+<h3>Properties</h3>
+<dl>
+	<dt><code>content<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `content` region controller object.</dd>
+	<dt><code>menu<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `menu` region controller object.</dd>
+	<dt><code>title<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `title` region object</dd>
+	<dt><code>toolbar<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `toolbar` region object</dd>
+	<dt><code>router<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `router` region object</dd>
+	<dt><code>views<span class="inheritance-info">inherited from wp.media.view.Frame</span></dt>
+	<dd>The `router` region object</dd>
+</dl>
+<h3>Methods</h3>
+<dl>
+	<dt><code>state( state )</code><span class="inheritance-info">inherited from wp.media.controller.StateMachine</span></dt>
+	<dd>Get a state object. <br>Defaults to the current state; if a state ID is supplied, returns that state.</dd>
+	<dt><code>setState( state )</code><span class="inheritance-info">inherited from wp.media.controller.StateMachine</span></dt>
+	<dd>Set the state.<br>
+		Triggers an `activate` event on the state, and a `deactivate` event on the previous state.</dd>
+	<dt><code>lastState()</code><span class="inheritance-info">inherited from wp.media.controller.StateMachine</span></dt>
+	<dd>Get the previously active state object.</dd>
+</dl>
 <div class="example">
 	<h3>Example: Render a view with a subview</h3>
 	<h4>LIVE EXAMPLE <a class="add-new-h2" target="_blank" href="<?php echo WPMT::get_example_url( WPMT::get_current_section(), 1 ) ?>">open in a new window</a></h4>
