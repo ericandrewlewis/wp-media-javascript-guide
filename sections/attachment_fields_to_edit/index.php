21e68b63ae7ad3bb077acf196648407bf666cb47
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-10-26 09:21:49 -0700

    attachment_fields_to_edit outbound link

---
+++ sections/attachment_fields_to_edit/index.php
@@ -0,0 +1,3 @@
+<h3>attachment_fields_to_edit</h3>
+<p>A filter that allows for developers to add custom UI elements to an attachment details view.</p>
+<p>See the <a href="http://codex.wordpress.org/Plugin_API/Filter_Reference/attachment_fields_to_edit">codex page</a>.</p>
